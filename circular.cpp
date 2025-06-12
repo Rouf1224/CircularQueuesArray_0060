cfab6854b47c664626c89be140ec3b21ac6b4618
Rouf1224 <ahmadroufr24@gmail.com>
2025-06-12 18:35:25 +0700

    menambahkan data pada void remove iteras

--- circular.cpp
+++ circular.cpp
@@ -97,6 +97,15 @@ class Queues
                 cout << queue_array[FRONT_position] << " ";
                 FRONT_position++;
             }
+
+            FRONT_position = 0;
+
+            while (FRONT_position <= REAR_position)
+            {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+            cout << endl;
         }
     }
 
