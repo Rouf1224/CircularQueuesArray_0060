26711eb6cf359d1df49f30dcc3cc0a35009d85ee
Rouf1224 <ahmadroufr24@gmail.com>
2025-06-12 17:42:49 +0700

    menambahkan data

--- circular.cpp
+++ circular.cpp
@@ -22,10 +22,16 @@ class Queues
     cin >> num;
     cout << endl;
 
-    if ((FRONT == 0 && REAR == max -1)) || (FRONT == REAR + 1)
+    if ((FRONT == 0 && REAR == max -1)  || (FRONT == REAR + 1))
     {
       cout << "\nQueue overflow\n";
+      return;
     }
-
+    if (FRONT == -1)
+    {
+        FRONT = 0;
+        REAR = 0; 
+    }
+    
    }
 }
